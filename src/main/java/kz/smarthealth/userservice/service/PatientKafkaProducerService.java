eaa28f5d02aff313f4a1c3aabfb0de933c1cad33
a.samat.dev <a.samat.dev@gmail.com>
2023-06-07 18:57:37 +0600

    Patient creation implemented

--- src/main/java/kz/smarthealth/userservice/service/PatientKafkaProducerService.java
+++ src/main/java/kz/smarthealth/userservice/service/PatientKafkaProducerService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kz.smarthealth.userservice.aop.Log;
 import kz.smarthealth.userservice.model.dto.PatientDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -19,20 +20,20 @@ import java.util.concurrent.CompletableFuture;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PatientKafkaProducerService {
 
-//    @Value("${kafka.topic.new-patients}")
-//    private final String topicName;
+    @Value("${kafka.topic.new-patients}")
+    private String topicName;
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
 
     @Log
     public void sendMessage(PatientDTO patientDTO) throws JsonProcessingException {
         String message = objectMapper.writeValueAsString(patientDTO);
-        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("user-service-new-patients", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
 
-        future.whenComplete((key, value) -> {
-            System.out.println("Patient: " + key + " - " + value);
-        });
+        future.whenComplete((key, value) -> log.info("Newly created patient sent to patient service: {}",
+                patientDTO.toString()));
     }
 }
